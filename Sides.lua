1b3e1082a4ea66067accb9bd92a6a5ff3dcdd3b9
P100sch <ph.100sch@gmail.com>
2020-02-15 23:34:27 +0100

    Update Sides.lua
    Sides API documentation changes

--- Sides.lua
+++ Sides.lua
@@ -1,8 +1,8 @@
----@class Sides : table
----@field public bottom number
----@field public top number
----@field public back number
----@field public front number
----@field public left number
----@field public right number
+---@class Sides : table Has a indexer to convert numbers back to strings
+---@field public bottom number Number:0
+---@field public top number Number:1
+---@field public back number Number:2
+---@field public front number Number:3
+---@field public left number Number:4
+---@field public right number Number:5
 local sides = require("sides")
