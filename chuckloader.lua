fa95cc2d8606783f8a036ade5afae9d58fcd64c8
P100sch <ph.100sch@gmail.com>
2020-02-16 01:33:32 +0100

    Create chuckloader.lua
    Chunkloader Component API

---
+++ chuckloader.lua
@@ -0,0 +1,4 @@
+---@class Chunkloader : ComponentProxy
+---@field isActive fun():boolean Returns whether the chunkloader is currently active.
+---@field setActive fun(enabled:boolean):boolean Enables or disables the chunkloader. Returns the new state, which may be false if no chunk loading ticket could be acquired.
+local chunkloader = require("component").chunkloader
